3ea6861e8691d99095cbd610928587aea0efc7ff
kuveee <kamilavillablanca23@gmail.com>
2018-01-31 11:48:23 -0300

    actualizando index

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 //Eventos 
 
-// Detonador evento en CVV
-let inputcvv= document.getElementsByName('cvv');
+//detonador evento en CVV 
+let inputcvv= document.getElementsByName('cvv'); 
 inputcvv[0].addEventListener('keyup', validInputCVV);
 
 //detonador evento de tarjeta 
-let inputcn= document.getElementsByName('cn')
+let inputcn= document.getElementsByName('cn') 
 inputcn[0].addEventListener('keyup', validInputTarget);
 
 //detonador de validacion completa
 let btn= document.getElementsByTagName('input');
 let positionInput= btn.length -1;
-btn[positionInput].addEventListener('click', isValidForm);
+btn[positionInput].addEventListener('click', isValidForm); 
 
 
 //funciones  
 function validInputTarget(event){ 
-	let numberCard= document.getElementsByName('cn')[0]; 
+  let numberCard= document.getElementsByName('cn')[0]; 
   let key= event.keyCode;  
   if(key>=48 && key<=57 || key==8){
     if(cn.value.length>18){
@@ -25,10 +25,10 @@ function validInputTarget(event){
   } else{
     cn.value= cn.value.substr(0,-1); 
   }  
-}
+}; 
 
 function validInputCVV(event){  
-	let cvv= document.getElementsByName('cvv')[0];
+  let cvv= document.getElementsByName('cvv')[0];
   let key= event.keyCode; 
   if(key>=48 && key<=57 || key==8){
     if(cvv.value.length>3){
@@ -38,40 +38,40 @@ function validInputCVV(event){
     cvv.value= cvv.value.substr(0,0); 
   }  
 
-}
+};
 
 function isValidCard(numberCard){ 
   if (numberCard.length === 0) {
       alert('Ingrese un número');
     } else {
-      const arr = [];
-      for (let i = 0; i < numberCard.length; i++) {
+      var arr = [];
+      for (var i = 0; i < numberCard.length; i++) {
         arr.push(numberCard[i]);
-      } for (let elem = 0; elem < arr.length; elem++) {
-        const item = arr.pop();
+      } for (var elem = 0; elem < arr.length; elem++) {
+        var item = arr.pop();
         arr.splice(elem, 0, item);
         var num = [];
-      } for (let x = 0; x < arr.length; x++) {
+      } for (var x = 0; x < arr.length; x++) {
         num.push(parseInt(arr[x]));
         var pairPosition = [];
         var impairPosition = 0;
         var result;
       // Separar los números en posiciones pares
-      } for (let p = 0; p < num.length; p ++) {
+      } for (var p = 0; p < num.length; p ++) {
         if ([ p + 1] % 2 === 0) {
           pairPosition.push((num[p]) * 2);
         } else if ([p + 1] % 2 !== 0)
           impairPosition += (num[p]);
         var pairPositionLess = 0;
         var pairPositionHigth = [];
-      } for (let s = 0; s < pairPosition.length; s++) {
+      } for (var s = 0; s < pairPosition.length; s++) {
         if (pairPosition[s] < 10) {
           pairPositionLess += (pairPosition[s]);
         } else {
           pairPositionHigth.push(pairPosition[s]);
-          const pairString = pairPositionHigth.join('');
+          var pairString = pairPositionHigth.join('');
           var sum = 0;
-          for (let y = 0; y < pairString.length; y++) {
+          for (var y = 0; y < pairString.length; y++) {
             sum += parseInt(pairString[y]);
           }
         }
@@ -90,8 +90,8 @@ function isValidCard(numberCard){
 }
 
 function isValidDate(){
-	let inputDate= document.getElementsByName('exp')[0];
-	let date= document.getElementsByName('exp')[0].value;
+  let inputDate= document.getElementsByName('exp')[0];
+  let date= document.getElementsByName('exp')[0].value;
   let fecha= new Date(); 
   let actualMonth= fecha.getMonth()+1; 
   let actualYear= fecha.getFullYear(); 
@@ -106,6 +106,7 @@ function isValidDate(){
   } else if (year >= shortYear && month >= actualMonth){
     inputDate.classList.remove('error'); 
     inputDate.classList.add('success'); 
+    return true; 
   }
 }
 
@@ -122,28 +123,30 @@ function isValidCVV(cvv){
 }
 
 function isValidName(nombre){
-	let space= nombre.value.indexOf(' ');
-	let firstName= nombre.value.substr(0,space);
-	let lastName= nombre.value.substr(space+1);
-	if(firstName.length >=2 && lastName.length>=2){
-		 nombre.classList.remove('error'); 
-		 nombre.classList.add('success');
-		 return true;  
-	} else{
-		nombre.classList.remove('success'); 
-		nombre.classList.add('error'); 
-		return false; 
-	}
+  let space= nombre.value.indexOf(' ');
+  let firstName= nombre.value.substr(0,space);
+  let lastName= nombre.value.substr(space+1);
+  if(firstName.length >=2 && lastName.length>=2){
+     nombre.classList.remove('error'); 
+     nombre.classList.add('success');
+     return true;  
+  } else{
+    nombre.classList.remove('success'); 
+    nombre.classList.add('error'); 
+    return false; 
+  }
 }
 //validacion completa
 function isValidForm(event){
-	let numberCard= document.getElementsByName('cn')[0].value;
-	let cvv=document.getElementsByName('cvv')[0];
-	let nombre= document.getElementsByName('name')[0]; 
+  let numberCard= document.getElementsByName('cn')[0].value;
+  let cvv=document.getElementsByName('cvv')[0];
+  let nombre= document.getElementsByName('name')[0]; 
   event.preventDefault();  
   isValidCard(numberCard); 
   isValidDate();
-  isValidCVV(); 
+  isValidCVV(cvv); 
+  isValidName(nombre);
+
 }
 
 module.exports = isValidCard;
